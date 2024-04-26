6198e9e888b2d3b5fa3218a81bc4bdd5c077791e
Eric Lam <voidful.stack@gmail.com>
2024-04-27 07:14:46 +0800

    Update preprocess.py

--- mixed/preprocess.py
+++ mixed/preprocess.py
@@ -1,19 +1,22 @@
 from datasets import load_dataset, concatenate_datasets
-import nlp2
+import argparse
 
-data_list = []
-for dataset_name in nlp2.get_files_from_dir('./dataset', match='jsonl'):
-    print(dataset_name)
-    data_list.append(load_dataset("json", data_files=dataset_name)['train'])
+def load_and_concatenate_data():
+    data_list = []
+    for dataset_name in nlp2.get_files_from_dir('./dataset', match='jsonl'):
+        print(dataset_name)
+        data_list.append(load_dataset("json", data_files=dataset_name)['train'])
+    ds = concatenate_datasets(data_list)
+    ds = ds.shuffle()
+    print(ds, ds[0])
+    return ds
 
-ds = concatenate_datasets(data_list)
-ds.shuffle()
-print(ds, ds[0])
+def add_hash(ds):
+    return ds.map(lambda example: {"hash": hash(str(example["chat"]))})
 
-
-def get_hash(example):
-    return {"hash": hash(str(example["chat"]))}
-
+def filter_unique(ds):
+    uniques = set(ds.unique("hash"))
+    return ds.filter(lambda example, uniques=uniques: check_uniques(example, uniques), batched=True)
 
 def check_uniques(example, uniques):
     if example["hash"] in uniques:
@@ -22,10 +25,14 @@ def check_uniques(example, uniques):
     else:
         return False
 
+def main(dataset_name):
+    ds = load_and_concatenate_data()
+    ds = add_hash(ds)
+    ds = filter_unique(ds)
+    ds.push_to_hub(dataset_name, private=True)
 
-ds = ds.map(get_hash)
-uniques = set(ds.unique("hash"))
-ds_filter = ds.filter(check_uniques, fn_kwargs={"uniques": uniques})
-print(ds_filter)
-
-ds_filter.push_to_hub('voidful/mixed_inst_multi_chat', private=True)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process and push dataset to Hugging Face's Dataset Hub.")
+    parser.add_argument('dataset_name', type=str, help='The name of the dataset repository on Hugging Face Hub')
+    args = parser.parse_args()
+    main(args.dataset_name)
